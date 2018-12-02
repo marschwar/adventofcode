849588e6491a1e917c9f0e38bc3925cb6bbed4f8
Markus Schwarz <post@markus-schwarz.net>
2018-12-02 13:42:44 +0100

    [day 2] using operator fun to add pairs

--- src/main/kotlin/de/codekenner/aoc2018/Day2.kt
+++ src/main/kotlin/de/codekenner/aoc2018/Day2.kt
@@ -2,20 +2,19 @@ package de.codekenner.aoc2018
 
 class Day2 : Puzzle() {
     override fun part1(input: Sequence<String>): Any {
-        val stats = input.map(String::asSequence)
+        val stats = input
             .map { it.groupBy { it } }
             .map {
-                val doubles = it.values.any { chars -> chars.size == 2 }
-                val triples = it.values.any { chars -> chars.size == 3 }
+                val doubles = if (it.values.any { chars -> chars.size == 2 }) 1 else 0
+                val triples = if (it.values.any { chars -> chars.size == 3 }) 1 else 0
                 Pair(doubles, triples)
             }
-            .fold(0 to 0) { acc, stats ->
-                val doubles = acc.first + (if (stats.first) 1 else 0)
-                val triples = acc.second + (if (stats.second) 1 else 0)
-                doubles to triples
-            }
+            .fold(0 to 0) { acc, stats -> acc + stats }
         return stats.first * stats.second
     }
+
+    private operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> =
+        first + other.first to second + other.second
 
     override fun part2(input: Sequence<String>): Any {
         val inList = input.toList()
