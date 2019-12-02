17b9193830baa8b8507f631ea08eea3d8de2793a
Markus Schwarz <post@markus-schwarz.net>
2019-12-02 22:08:23 +0100

    2019/02b

--- src/main/kotlin/de/marschwar/adventofcode/v2019/Day2.kt
+++ src/main/kotlin/de/marschwar/adventofcode/v2019/Day2.kt
@@ -6,16 +6,29 @@ class Day2(private val replace: Boolean = true) : Puzzle() {
 
     override fun part1(input: Sequence<String>): Any {
         val program = input.toProgram()
-        if (replace) {
-            program.set(1, 12)
-            program.set(2, 2)
-        }
-        execute(program)
-        return program.first()
+        return if (replace)
+            program.replaceAndRun(12, 2)
+        else
+            program.doRun()
     }
 
     override fun part2(input: Sequence<String>): Any {
-        return ""
+        val programTemplate = input.toProgram()
+        (0..99).forEach { noun ->
+            (0..99).forEach { verb ->
+                val program = programTemplate.toMutableList()
+                if (program.replaceAndRun(noun, verb) == 19690720) {
+                    return 100 * noun + verb
+                }
+            }
+        }
+        return "no result"
+    }
+
+    private fun Program.replaceAndRun(nown: Int, verb: Int): Int {
+        set(1, nown)
+        set(2, verb)
+        return doRun()
     }
 
     private fun Sequence<String>.toProgram(): Program =
@@ -24,21 +37,21 @@ class Day2(private val replace: Boolean = true) : Puzzle() {
             .map(String::toInt)
             .toMutableList()
 
-    private tailrec fun execute(program: Program, currentIndex: Int = 0) {
-        val instruction = program.get(currentIndex)
+    private tailrec fun Program.doRun(currentIndex: Int = 0): Int {
+        val instruction = get(currentIndex)
         if (instruction == 99) {
-            return
+            return first()
         }
-        val left = program.get(program.get(currentIndex + 1))
-        val right = program.get(program.get(currentIndex + 2))
-        val targetIndex = program.get(currentIndex + 3)
+        val left = get(get(currentIndex + 1))
+        val right = get(get(currentIndex + 2))
+        val targetIndex = get(currentIndex + 3)
         val newValue = when (instruction) {
             1 -> left + right
             2 -> left * right
             else -> error("unexpected instruction $instruction")
         }
-        program.set(targetIndex, newValue)
-        execute(program, currentIndex + 4)
+        set(targetIndex, newValue)
+        return doRun(currentIndex + 4)
     }
 }
 
