351ecf751295fff5206ff6cc258cb3d4010b2174
Markus Schwarz <markus.schwarz@otto.de>
2021-12-03 08:57:09 +0100

    2021-02

---
+++ aoc2021/src/main/kotlin/de/marschwar/adventofcode/Day2.kt
@@ -0,0 +1,62 @@
+package de.marschwar.adventofcode
+
+class Day2 : Puzzle() {
+
+    override fun part1(input: Sequence<String>): Any {
+        return input
+            .map(this::parsed)
+            .fold(PositionPart1()) { acc, v -> acc.apply(v.first, v.second) }
+            .calculateResult()
+
+    }
+
+    override fun part2(input: Sequence<String>): Any {
+        return input
+            .map(this::parsed)
+            .fold(PositionPart2()) { acc, v -> acc.apply(v.first, v.second) }
+            .calculateResult()
+    }
+
+    private fun parsed(instruction: String): Pair<String, Int> {
+        val (operation, amountAsString) = INSTRUCTION_REGEX.matchEntire(instruction)?.destructured
+            ?: error("no match for $instruction")
+        return operation to amountAsString.toInt()
+    }
+
+    private class PositionPart1(var depth: Int = 0, var horizontal: Int = 0) {
+
+        fun apply(operation: String, amount: Int): PositionPart1 {
+            when (operation) {
+                "up" -> depth -= amount
+                "down" -> depth += amount
+                "forward" -> horizontal += amount
+                else -> error("unknown operation $operation")
+            }
+            return this
+        }
+        fun calculateResult(): Int = depth * horizontal
+    }
+
+    private class PositionPart2(var depth: Int = 0, var horizontal: Int = 0, var aim: Int = 0) {
+        fun apply(operation: String, amount: Int): PositionPart2 {
+            when (operation) {
+                "up" -> aim -= amount
+                "down" -> aim += amount
+                "forward" -> {
+                    horizontal += amount
+                    depth += (aim * amount)
+                }
+                else -> error("unknown operation $operation")
+            }
+            return this
+        }
+
+        fun calculateResult(): Int = depth * horizontal
+    }
+
+    companion object {
+        private val INSTRUCTION_REGEX = """(\w+) (\d+)""".toRegex()
+    }
+}
+
+fun main() = Day2().solve()
---
+++ aoc2021/src/test/kotlin/de/marschwar/adventofcode/Day2Test.kt
@@ -0,0 +1,34 @@
+package de.marschwar.adventofcode
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+
+internal class Day2Test {
+
+    val puzzle = Day2()
+    val inputs = sequenceOf(
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2",
+    )
+
+    @Test
+    fun part1() {
+
+        val result = puzzle.part1(inputs)
+
+        assertThat(result).isEqualTo(150)
+    }
+
+    @Test
+    fun part2() {
+
+        val result = puzzle.part2(inputs)
+
+        assertThat(result).isEqualTo(900)
+    }
+
+}
