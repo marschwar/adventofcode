4afb0ed74d7054693ae1ad5f8b9367b448c2d9e4
Markus Schwarz <post@markus-schwarz.net>
2021-12-09 08:52:32 +0100

    2021 - Day 9 - part 1 (refactor)

--- aoc2021/src/main/kotlin/de/marschwar/adventofcode/Aoc2021Day9.kt
+++ aoc2021/src/main/kotlin/de/marschwar/adventofcode/Aoc2021Day9.kt
@@ -2,30 +2,49 @@ package de.marschwar.adventofcode
 
 internal class Aoc2021Day9 : Puzzle() {
     override fun part1(input: PuzzleInput): Any {
-        val itemsPerRow = input.first().count()
-        val numbers = input
-            .flatMap { s -> s.chunked(1).map(String::toByte) }
-            .toList()
-        return numbers.filterIndexed { index, i ->
-            numbers.adjacentTo(itemsPerRow, index).all { it > i }
-        }.sumOf { it + 1 }
+        return HeightMap.parse(input)
+            .findLowPoints()
+            .sumOf { it + 1 }
     }
 
-    private fun List<Byte>.adjacentTo(itemsPerRow: Int, index: Int): List<Byte> {
-        val others = mutableListOf<Byte>()
-        if (index.mod(itemsPerRow) > 0) {
-            others.add(this[index - 1])
+    private class HeightMap(private val itemsPerRow: Int, private val heights: List<Byte>) {
+
+        fun findLowPoints(): List<Byte> {
+            return findLowIndexes().map { heights[it] }
         }
-        if ((index + 1).mod(itemsPerRow) > 0) {
-            others.add(this[index + 1])
+
+        fun findLowIndexes(): List<Int> {
+            return heights.mapIndexed { index, byte ->
+                if (adjacentTo(index).all { it > byte }) index else null
+            }.filterNotNull()
         }
-        if (index >= itemsPerRow) {
-            others.add(this[index - itemsPerRow])
+
+        fun adjacentTo(index: Int): List<Byte> {
+            val others = mutableListOf<Byte>()
+            if (index.mod(itemsPerRow) > 0) {
+                others.add(heights[index - 1])
+            }
+            if ((index + 1).mod(itemsPerRow) > 0) {
+                others.add(heights[index + 1])
+            }
+            if (index >= itemsPerRow) {
+                others.add(heights[index - itemsPerRow])
+            }
+            if (index < heights.size - itemsPerRow) {
+                others.add(heights[index + itemsPerRow])
+            }
+            return others
         }
-        if (index < size - itemsPerRow) {
-            others.add(this[index + itemsPerRow])
+
+        companion object {
+            fun parse(input: Sequence<String>): HeightMap {
+                val itemsPerRow = input.first().count()
+                val numbers = input
+                    .flatMap { s -> s.chunked(1).map(String::toByte) }
+                    .toList()
+                return HeightMap(itemsPerRow, numbers)
+            }
         }
-        return others
     }
 }
 
